b5d1801371e366ff5e84a8e490f182320c1b0976
Amirhossein <anejadkoorki8199@gmail.com>
2021-11-22 00:02:34 +0330

    some updates to code .

--- ProductBarcoding/barcode_app/models.py
+++ ProductBarcoding/barcode_app/models.py
@@ -2,10 +2,8 @@ from django.db import models
 from django.core.validators import RegexValidator
 from smart_selects.db_fields import ChainedForeignKey
 
-
 from .categories_models import MotherCategory, SecondCategory, ThirdCategory
 from .other_models import Store, Size, Colour
-
 
 five_digit_validator = RegexValidator(
     regex="^[0-9]{4}[1-9]{1}$",
@@ -65,10 +63,12 @@ class Product(models.Model):
         max_length=200,
     )
 
+    def get_barcode(self):
+        return f"{self.mother_category.code}-{self.second_category.code}-{self.third_category.code}-" \
+               f"{self.code}-{self.colour.code}-{self.size.code}-{self.store.code}"
+
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
         unique_together = ['name', 'code']
-
-
--- ProductBarcoding/barcode_app/views.py
+++ ProductBarcoding/barcode_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
@@ -34,4 +35,37 @@ class CreateProduct(CreateView):
         )
 
     def form_valid(self, form):
-        pass
+        cleaned_data = form.cleaned_data
+
+        prod_last_object = models.Product.objects.last()
+        if prod_last_object:
+            last_prod_code = int(prod_last_object.code)
+            new_prod_code_int = last_prod_code + 1
+            new_prod_code = str(new_prod_code_int).zfill(5)
+            new_obj = models.Product.objects.create(
+                code=new_prod_code,
+                mother_category=cleaned_data.get('mother_category'),
+                second_category=cleaned_data.get('second_category'),
+                third_category=cleaned_data.get('third_category'),
+                colour=cleaned_data.get('colour'),
+                size=cleaned_data.get('size'),
+                store=cleaned_data.get('store'),
+                name=cleaned_data.get('name'),
+            )
+            messages.success(request=self.request,
+                             message=f"Product has been saved successfully with barcode: {new_obj.get_barcode()}")
+            return redirect('barcode_app:create-product')
+        else:
+            new_obj = models.Product.objects.create(
+                code='00001',
+                mother_category=cleaned_data.get('mother_category'),
+                second_category=cleaned_data.get('second_category'),
+                third_category=cleaned_data.get('third_category'),
+                colour=cleaned_data.get('colour'),
+                size=cleaned_data.get('size'),
+                store=cleaned_data.get('store'),
+                name=cleaned_data.get('name'),
+            )
+            messages.success(request=self.request,
+                             message=f"Product has been saved successfully with barcode: {new_obj.get_barcode()}")
+            return redirect('barcode_app:create-product')
